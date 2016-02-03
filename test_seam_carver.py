6ce0a420d2aa3ad5dfc3f7c32ab53a0625beacdb
Margaret Sy <margaretsy1016@gmail.com>
2016-02-03 12:45:49 -0800

    clean up test file

--- test_seam_carver.py
+++ test_seam_carver.py
@@ -1,15 +1,20 @@
+import numpy as np
+from PIL import Image
 import unittest
-from seam_carver import *
+from seam_carver import (
+	get_img_arr
+)
 
 class UtilsTestCase(unittest.TestCase):
 
 	def setUp(self):
 		# from the princeton site's project instructions
-		self.test_img = np.array(
+		self.test_img = np.array([
 			[[255, 101, 51], [255, 101, 153], [255, 101, 255]],
 			[[255, 153, 51], [255, 153, 153], [255, 153, 255]],
 			[[255, 203, 51], [255, 204, 153], [255, 205, 255]],
-			[[255, 255, 51], [255, 255, 153], [255, 255, 255]])
+			[[255, 255, 51], [255, 255, 153], [255, 255, 255]]
+		])
 
 	def test_import_square(self):
 		square_img = get_img_arr("imgs/mountain_icon.jpg")
